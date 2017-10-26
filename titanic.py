5f0d86506182b829f2e49eef51014c8aeee2cf49
syamkumar <syammbits@gmail.com>
2017-10-26 13:45:47 +0530

    filtered data

--- titanic.py
+++ titanic.py
@@ -4,5 +4,6 @@ df = pd.read_excel(source_dir+'\\titanic3.xls')
 
 cols=df.columns.values.tolist()
 
-
+df['survived']=df['survived'].replace(1,'Yes')
+df['survived']=df['survived'].replace(0,'No')
 
